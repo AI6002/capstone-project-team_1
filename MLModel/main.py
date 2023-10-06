93936688917bf792f818428f24ecdc6ceb61369c
mozaffari-sadaf <mozaffari.sadaf@gmail.com>
2023-10-06 19:16:43 -0230

    Add preprocessing functionality

--- MLModel/main.py
+++ MLModel/main.py
@@ -5,19 +5,44 @@ import pandas as pd
 
 
 def review_analysis(df):
-    # TODO: Preprocessing
+    """
+    Analyses the opinions toward the product.
+
+    Args:
+        df: A Pandas DataFrame containing the reviews.
+
+    Returns:
+        A Pandas DataFrame containing the preprocessed reviews.
+    """
+
+    review = Preprocess(df)
+    preprocessed_data = review.preprocess_data()
 
     # TODO: Aspect extraction
 
     # TODO: Sentiment Analysis
 
-    return
+    return preprocessed_data
 
 
 if __name__ == "__main__":
-    # Read review data
-    # df = pd.read_csv('./data/dataset.csv')
 
-    # Analyse the opinions toward the product
-    # review_analysis(df)
-    pass
+    reviews = []  # Initialize with an empty list
+
+    # Read data from the text file (each line is treated as a separate entry)
+    try:
+        with open('./MLModel/data/ScrapedData.txt', 'r', encoding='utf-8') as file:
+            reviews = file.readlines()
+    except FileNotFoundError:
+        print("The input file does not exist.")
+    except IOError as e:
+        print(f"An error occurred while reading the input file: {str(e)}")
+
+    if reviews:
+        # Create a DataFrame from the reviews
+        df = pd.DataFrame({'Review': reviews})
+
+        # Analyse the opinions toward the product
+        print(review_analysis(df))
+    else:
+        print("No reviews were loaded. Exiting the program.")
