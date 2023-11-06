709e92775ab28c5abecc5debff74d99740b14296
Vahid Khoddami <vahid.khoddami@gamil.com>
2023-11-05 22:52:45 -0330

    Add PyABSA aspect extractor and trainer

---
+++ MLModel/PyABSA/auto_anotate.py
@@ -0,0 +1,3 @@
+from pyabsa import make_ABSA_dataset 
+# refer to the comments in this function for detailed usage
+make_ABSA_dataset(dataset_name_or_path='mlmodel/pyabsa/train_70.csv.ignore', checkpoint='english')
