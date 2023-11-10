ca6df0067e6ff0db78a4f4758a9c8ed7d4672c86
Vahid Khoddami <vahid.khoddami@gamil.com>
2023-11-09 23:54:41 -0330

    Refactor code to be more modular

--- MLModel/PyABSA/extract_aspects.py
+++ MLModel/PyABSA/extract_aspects.py
@@ -4,34 +4,43 @@
 # author: yangheng <hy345@exeter.ac.uk>
 # github: https://github.com/yangheng95
 # Copyright (C) 2021. All Rights Reserved.
-from pyabsa import ABSADatasetList, available_checkpoints
 from pyabsa import ATEPCCheckpointManager
 
-# checkpoint_map = available_checkpoints(from_local=False)
+async def extract_aspects_and_sentiments(path_to_data: str, checkpoint: str):
 
+    # load data from a text file line by line in a list
+    data_to_analyze = []
+    with open(path_to_data, 'r', encoding='utf-8') as f:
+        for line in f:
+            data_to_analyze.append(line)
 
-aspect_extractor = ATEPCCheckpointManager.get_aspect_extractor(
-    checkpoint="english",
+    inference_source = data_to_analyze
+
+    # Load the model
+    aspect_extractor = ATEPCCheckpointManager.get_aspect_extractor(
+    checkpoint=checkpoint,
     auto_device=True,  # False means load model on CPU
     cal_perplexity=True,
-)
-
-# load data from a text file line by line in a list
-scraped_data = []
-#with open("mlmodel/data/Train_70_LinebyLine.txt", 'r') as f:
-with open("mlmodel/data/sample_scraped_data_linebyline.txt", 'r') as f:
-    for line in f:
-        scraped_data.append(line)
-inference_source = examples = scraped_data
-print(inference_source)
-atepc_result = aspect_extractor.extract_aspect(
-    inference_source=inference_source,  #
+    )
+    
+    # Predict
+    atepc_result = aspect_extractor.extract_aspect(
+    inference_source=inference_source, 
     save_result=True,
     print_result=True,  # print the result
     pred_sentiment=True,  # Predict the sentiment of extracted aspect terms
-)
+    )
 
-for example in atepc_result:
-    print(example['aspect'], example['sentiment'])
+    result=[]
+    for example in atepc_result:
+        aspect_sentiment_pairs = {
+        'aspects': example['aspect'],
+        'sentiments': example['sentiment']
+        }
+        result.append(aspect_sentiment_pairs)
 
-#print(atepc_result)
+    return result
+
+if __name__ == '__main__':
+    import asyncio
+    print(asyncio.run(extract_aspects_and_sentiments("mlmodel/data/scraped_data.txt","MLModel/PyABSA/checkpoints/finetuned_model")))
