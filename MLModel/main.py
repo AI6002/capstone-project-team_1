baea2ef4891cbf0df834dc59f5d087c037d84c28
mozaffari-sadaf <mozaffari.sadaf@gmail.com>
2023-12-10 11:09:21 -0330

    Make the evaluation phase optional

--- MLModel/main.py
+++ MLModel/main.py
@@ -10,18 +10,10 @@ from MLModel.preprocessing import Preprocess
 from MLModel.aspect_mapping import map_reviews_to_synonyms
 from MLModel.feature_sentiment_analysis import analyze_feature_sentiments
 from MLModel.PyABSA.extract_aspects import extract_aspects_from_file
+from MLModel.evaluation import map_and_evaluate
 
-# Navigate to the root directory by going up one level (../)
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-# Add the root directory to the Python path
-sys.path.append(root_dir)
-from MLModel.preprocessing import Preprocess
-from MLModel.aspect_mapping import map_reviews_to_synonyms
-from MLModel.feature_sentiment_analysis import analyze_feature_sentiments
-from MLModel.PyABSA.extract_aspects import extract_aspects_from_file
-from evaluation import map_and_evaluate
 
-async def review_analysis(input_file_path, evaluation_file_path):
+async def review_analysis(input_file_path, evaluate=False):
     """
     Analyze the opinions toward the product based on a DataFrame of reviews.
 
@@ -50,41 +42,45 @@ async def review_analysis(input_file_path, evaluation_file_path):
     if not reviews:
         print("No reviews were loaded. Exiting the program.")
         sys.exit(0)
-        
-    # Call the function from the aspect_extraction module
-    # file_path = "./MLModel/data/sample_scraped_data_linebyline.txt"
+
     reviews = extract_aspects_from_file(output_file_path)
 
     # Clear GPU memory
     torch.cuda.empty_cache()
 
-    # TODO: Get the type of the product and pass it to the aspect mapping function
     # Call the function from the aspect_mapping module
-    mapped_reviews = map_reviews_to_synonyms(reviews, "laptop")
+    print("Found category: ", category)
+    print(reviews)
+    mapped_reviews = map_reviews_to_synonyms(reviews, category)
+    print(
+        "#############################################################################################################")
 
     # Now you can work with the mapped_reviews
     print(mapped_reviews)
 
-    # Concatenate rows of the evaluation data and get the result as a DataFrame
-    concatenated_data = prep.concat_rows_by_length(input_csv_path=evaluation_file_path, max_length=512)
-    # Evaluation
-    map_and_evaluate(mapped_reviews, concatenated_data)
+    if evaluate:
+        evaluation_file_name = input('Enter the evaluation file name')
+        evaluation_file_path = "./Data/Evaluation/" + evaluation_file_name
+        # Concatenate rows of the evaluation data and get the result as a DataFrame
+        concatenated_data = prep.concat_rows_by_length(input_csv_path=evaluation_file_path, max_length=128)
+
+        # Call the function
+        map_and_evaluate(mapped_reviews, concatenated_data)
 
     # Assuming you have already defined 'mapped_reviews' using the 'map_reviews_to_synonyms' function
     best_and_worst_features = analyze_feature_sentiments(mapped_reviews)
 
     print(best_and_worst_features)
-    
-    return best_and_worst_features
 
     return best_and_worst_features
 
 
-async def sentiment_analysis(input_file_path, evaluation_file_path):
-    return await review_analysis(input_file_path, evaluation_file_path)
+async def sentiment_analysis(input_file_path):
+    # Set the second argument as True if you need to do evaluation
+    return await review_analysis(input_file_path, False)
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     input_file_path = './MLModel/data/scraped_data.txt'
-    evaluation_file_path = './Data/Evaluation/Evaluation_data_laptop.csv'
-    loop.run_until_complete(sentiment_analysis(input_file_path, evaluation_file_path))
+    loop.run_until_complete(sentiment_analysis(input_file_path))
